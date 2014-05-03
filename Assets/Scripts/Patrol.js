ccc5c6bf47f324bb4c10ec36e8b2c3bc2061283a
DFectuoso <santiago1717@gmail.com>
2014-05-03 18:15:01 -0500

    added patrol point and enemies

---
+++ Assets/Scripts/Patrol.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var patrolPoints : Transform[];
+public var moveSpeed : float;
+
+private var currentPoint : int = 0;
+
+function Start () {
+	transform.position = patrolPoints[0].position;
+}
+
+function Update () {
+	
+	// Are we there yet?
+	if(transform.position == patrolPoints[currentPoint].position){
+		currentPoint++;
+		
+		if(currentPoint == patrolPoints.Length){
+			currentPoint = 0;
+		}
+	}	
+	
+	transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+
+}
