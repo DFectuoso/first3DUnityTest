2d58bf11c38f45081cecb7d964f64d1765bd7cea
DFectuoso <santiago1717@gmail.com>
2014-05-03 16:20:07 -0500

    Finished control move, adding materials to the main player

--- Assets/Scripts/PlayerMovement.js
+++ Assets/Scripts/PlayerMovement.js
@@ -1,9 +1,17 @@
 ﻿#pragma strict
 
+public var moveSpeed : float;
+public var maxSpeed : float = 5f;
+
+private var input : Vector3;
+
 function Start () {
 
 }
 
 function Update () {
-
+	if(rigidbody.velocity.magnitude < maxSpeed){
+		input = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		rigidbody.AddForce(input * moveSpeed);
+	}
 }
