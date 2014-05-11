b0db55cf90e701364c32b72c65e672cbb86cee5f
DFectuoso <santiago1717@gmail.com>
2014-05-10 19:30:44 -0500

    Adding chase script

---
+++ Assets/Scripts/Chase.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var targetEntity : GameObject;
+public var moveSpeed : float;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	
+			
+	transform.position = Vector3.MoveTowards(transform.position, targetEntity.transform.position, moveSpeed * Time.deltaTime);
+
+}
